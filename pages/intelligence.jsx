38a4d559a5212b3d7611710982d07a0bfc86ff78
anikhil511-source <anikhil511@gmail.com>
2026-06-10 07:23:10 +0530

    Update intelligence.jsx

--- pages/intelligence.jsx
+++ pages/intelligence.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Card from '@/components/Card';
+import Card from '@/component/components/Card';
 
 export default function Intelligence() {
   const [cards, setCards] = useState([]);
